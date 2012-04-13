45d6518d46bb3e0dab5f99ea35c47725a5c8bc97
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-04-14 01:29:34 +0400

    Separate KObjects for filesystem mount points
    FS module is maintaining list of mount points and appropriate KObjects
    with attributes in SysFS.
    Mount point's attributes are not separated yet, though.

--- kazenyuk/snapfs/snapfs-mgmt.c
+++ kazenyuk/snapfs/snapfs-mgmt.c
@@ -7,7 +7,13 @@
 
 #include "snapfs-mgmt.h"
 
+#define max_snapfs_mnt_points 10
+static int snapfs_mnt_point_number = -1;
+static struct snapfs_mnt_point snapfs_mnt_points[max_snapfs_mnt_points];
+#undef max_snapfs_mnt_points
+
 static char mnt_point_path[256];
+static struct kobject *snapfs_kobj = NULL;
 
 static ssize_t mnt_point_show(struct kobject *kobj, 
 				struct kobj_attribute *attr, char *buf)
@@ -44,8 +50,6 @@ static struct attribute *snapfs_mnt_attrs[] = {
 	NULL,
 };
 
-static struct kobject *snapfs_kobj = NULL;
-
 int setup_snapfs_mgmt()
 {
 	snapfs_kobj = kobject_create_and_add("snapfs", fs_kobj);
@@ -56,71 +60,43 @@ int setup_snapfs_mgmt()
 	return 0;
 }
 
-struct snapfs_mnt_point {
-	char *name;
-	struct kobject *kobj;
-	struct attribute_group *attrs;
-	struct dentry *dentry;
-};
-
-#define max_snapfs_mnt_points 5
-static int snapfs_mnt_point_number = -1;
-//static struct attribute_group snapfs_mnt_attr_grp[max_snapfs_mnt_points];
-//static struct kobject snapfs_mnt_point_kobjects[max_snapfs_mnt_points];
-static struct snapfs_mnt_point snapfs_mnt_points[max_snapfs_mnt_points];
-#undef max_snapfs_mnt_points
-
-int setup_snapfs_mount_point_mgmt(struct dentry *mnt_point_dentry)
+int setup_snapfs_mount_point_mgmt(struct dentry *dentry)
 {
 	int result;
+	const int mnt_point_name_length = 30;
 	struct snapfs_mnt_point *mnt_point;
-	struct attribute_group *mnt_point_grp;
-	struct kobject *mnt_point_kobj;
-	/*struct kobject *snapfs_mnt_point_kobj = kobject_create();
-	kobject_set_name(snapfs_mnt_point_kobj, "%d", number_of_mnt_points++);
-	kobject_init(snapfs_mnt_point_kojb, snapfs_mnt_point_ktype);
-	kobject_add(snapfs_mnt_point_kojb, fs_kobj, "%d", number_of_mnt_points - 1);*/
+	
 	++snapfs_mnt_point_number;
 	mnt_point = &snapfs_mnt_points[snapfs_mnt_point_number];
-	//mnt_point_grp = mnt_point->attrs;
-	//mnt_point_kobj = mnt_point->kobj;
 
+	mnt_point->name = kmalloc(mnt_point_name_length, GFP_KERNEL);
+	if (!mnt_point->name) {
+		printk(KERN_ERR "Can't do kmalloc\n");
+		return -ENOMEM;
+	}
+	snprintf(mnt_point->name, mnt_point_name_length, 
+			"mnt_point_%d", snapfs_mnt_point_number);
+	
+	mnt_point->kobj = kobject_create_and_add(mnt_point->name, snapfs_kobj);
+        if (!mnt_point->kobj) {
+		printk(KERN_ERR "Can't create SnapFS mount point KObject\n");
+		return -ENOMEM;
+	}
+	
 	mnt_point->attrs = kmalloc(sizeof(*(mnt_point->attrs)), GFP_KERNEL);
 	if (!mnt_point->attrs) {
 		printk(KERN_ERR "Can't do kmalloc\n");
 		return -ENOMEM;
 	}
-
-//	memset(mnt_point_grp, 0, sizeof(*mnt_point_grp));
-//	memset(mnt_point_kobj, 0, sizeof(*mnt_point_kobj));
+	memset(mnt_point->attrs, 0, sizeof(*mnt_point->attrs));
 	
-	/*mnt_point_grp->name = kmalloc(30, GFP_KERNEL); //maybe GFP_NOFS?
-	if (!mnt_point_grp->name) {
-		printk(KERN_ERR "Can't do kmalloc\n");
-		return -ENOMEM;
-	}*/
 	mnt_point->attrs->attrs = snapfs_mnt_attrs;
-	/*snprintf((char*)mnt_point_grp->name, 30, "mnt_point_%d", 
-		 snapfs_mnt_point_number);*/
-	mnt_point->dentry = mnt_point_dentry;
-	mnt_point->name = kmalloc(30, GFP_KERNEL);
-	if (!mnt_point->name) {
-		printk(KERN_ERR "Can't do kmalloc\n");
-		return -ENOMEM;
-	}
-	snprintf(mnt_point->name, 30, "mnt_point_%d", snapfs_mnt_point_number);
-
-	mnt_point->kobj = kobject_create_and_add(mnt_point->name, snapfs_kobj);
-	if (!mnt_point->kobj) {
-		printk(KERN_ERR "Can't create SnapFS mount point KObject\n");
-		return -ENOMEM;
-	}
-	result = sysfs_create_group(mnt_point_kobj, mnt_point->attrs);
+	mnt_point->dentry = dentry;
+	
+	result = sysfs_create_group(mnt_point->kobj, mnt_point->attrs);
 	if (result) {
 		printk(KERN_ERR "Can't create sysfs group");
-		//kobject_put(snapfs_kobj);
 	}
-	//++snapfs_mnt_point_number;
 	return result;
 }
 
