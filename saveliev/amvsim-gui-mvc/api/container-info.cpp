65339a0c063c2acb1e86c3cd1868322e2c692736
Vladislav Saveliev <vladislav.sav@gmail.com>
2012-04-13 16:59:09 +0400

    remove garbage

--- saveliev/amvsim-gui-mvc/api/container-info.cpp
+++
@@ -1,6 +0,0 @@
-#include "container-info.h"
-#include "StorageDescriptor.h"
-
-ContainerInfo::ContainerInfo(const std::string& name, const StorageDescriptor& descriptor, const ContainerState& state)
-    : {
-}
